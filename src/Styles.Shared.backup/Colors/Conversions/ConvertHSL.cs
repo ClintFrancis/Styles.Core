fd7e21e0cf710625d5e6f543a65ca05328a85930
Clinton Francis <clfranci@microsoft.com>
2017-03-23 13:03:47 +0000

    Fixed some funky setup issues from the NuGet template

---
+++ src/Styles.Shared.backup/Colors/Conversions/ConvertHSL.cs
@@ -0,0 +1,74 @@
+﻿using System;
+namespace Styles
+{
+	internal static class ConvertHSL
+	{
+		internal static IHsl ToColorSpace (IRgb color)
+		{
+			var result = ColorHSL.Empty;
+			var max = Math.Max (color.R, Math.Max (color.G, color.B));
+			var min = Math.Min (color.R, Math.Min (color.G, color.B));
+
+			// hue
+			if (max == min) {
+				result.H = 0; // undefined
+			} else if (max == color.R && color.G >= color.B) {
+				result.H = 60 * (color.G - color.B) / (max - min);
+			} else if (max == color.R && color.G < color.B) {
+				result.H = 60 * (color.G - color.B) / (max - min) + 360;
+			} else if (max == color.G) {
+				result.H = 60 * (color.B - color.R) / (max - min) + 120;
+			} else if (max == color.B) {
+				result.H = 60 * (color.R - color.G) / (max - min) + 240;
+			}
+
+			// luminance
+			result.L = (max + min) / 2;
+
+			// saturation
+			if (result.L == 0 || max == min) {
+				result.S = 0;
+			} else if (0 < result.L && result.L <= 0.5) {
+				result.S = (max - min) / (max + min);
+			} else if (result.L > 0.5) {
+				result.S = (max - min) / (2 - (max + min)); //(max-min > 0)?
+			}
+
+			return result;
+		}
+
+		// works fine
+		internal static IRgb ToColor (IHsl item)
+		{
+			if (item.S == 0) {
+				// achromatic color (gray scale)
+				return new ColorRGB (item.L, item.L, item.L, 1);
+			} else {
+				double q = (item.L < 0.5) ? (item.L * (1.0 + item.S)) : (item.L + item.S - (item.L * item.S));
+				double p = (2 * item.L) - q;
+
+				double Hk = item.H / 360;
+				double [] T = new double [3];
+				T [0] = Hk + (1f / 3f);     // Red
+				T [1] = Hk;                 // Blue
+				T [2] = Hk - (1f / 3f);     // Green
+
+				for (int i = 0; i < 3; i++) {
+					if (T [i] < 0) T [i] += 1;
+					if (T [i] > 1) T [i] -= 1;
+
+					if ((T [i] * 6) < 1) {
+						T [i] = p + ((q - p) * 6 * T [i]);
+					} else if ((T [i] * 2.0) < 1) {
+						T [i] = q;
+					} else if ((T [i] * 3.0) < 2) {
+						T [i] = p + (q - p) * ((2 / 3) - T [i]) * 6;
+					} else T [i] = p;
+				}
+
+				return new ColorRGB (T [0], T [1], T [2], 1);
+			}
+		}
+	}
+}
+
---
+++ src/Styles.Shared.backup/Colors/Conversions/ConvertXYZ.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace Styles
+{
+	internal static class ConvertXYZ
+	{
+		#region Constants/Helper methods for Xyz related spaces
+		internal static readonly IXyz WhiteReference = new ColorXYZ {
+			X = 95.047,
+			Y = 100.000,
+			Z = 108.883
+		};
+		internal const double Epsilon = 0.008856; // Intent is 216/24389
+		internal const double Kappa = 903.3; // Intent is 24389/27
+
+		internal static double CubicRoot (double n)
+		{
+			return Math.Pow (n, 1.0 / 3.0);
+		}
+		#endregion
+
+		internal static IXyz ToColorSpace (IRgb color)
+		{
+			// convert to a sRGB form
+			var r = (color.R > 0.04045) ? Math.Pow ((color.R + 0.055) / (1 + 0.055), 2.4) : (color.R / 12.92);
+			var g = (color.G > 0.04045) ? Math.Pow ((color.G + 0.055) / (1 + 0.055), 2.4) : (color.G / 12.92);
+			var b = (color.B > 0.04045) ? Math.Pow ((color.B + 0.055) / (1 + 0.055), 2.4) : (color.B / 12.92);
+
+			var result = ColorXYZ.Empty;
+			result.X = (r * 0.4124 + g * 0.3576 + b * 0.1805);
+			result.Y = (r * 0.2126 + g * 0.7152 + b * 0.0722);
+			result.Z = (r * 0.0193 + g * 0.1192 + b * 0.9505);
+
+			return result;
+		}
+
+		internal static IRgb ToColor (IXyz item)
+		{
+			// (Observer = 2°, Illuminant = D65)
+			var x = item.X;
+			var y = item.Y;
+			var z = item.Z;
+
+			var r = x * 3.2406 + y * -1.5372 + z * -0.4986;
+			var g = x * -0.9689 + y * 1.8758 + z * 0.0415;
+			var b = x * 0.0557 + y * -0.2040 + z * 1.0570;
+
+			r = r > 0.0031308 ? 1.055 * Math.Pow (r, 1 / 2.4) - 0.055 : 12.92 * r;
+			g = g > 0.0031308 ? 1.055 * Math.Pow (g, 1 / 2.4) - 0.055 : 12.92 * g;
+			b = b > 0.0031308 ? 1.055 * Math.Pow (b, 1 / 2.4) - 0.055 : 12.92 * b;
+
+			return ColorRGB.FromRGB (ToRgb (r), ToRgb (g), ToRgb (b));
+		}
+
+		private static int ToRgb (double n)
+		{
+			var result = 255.0 * n;
+			if (result < 0) return 0;
+			if (result > 255) return 255;
+			return (int)Math.Round (result);
+		}
+	}
+}
+
