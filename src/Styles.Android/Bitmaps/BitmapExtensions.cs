06ad6ae7a014af4f95d00d5dccd4aa2d0c8fa545
Clinton Francis <clfranci@microsoft.com>
2017-06-28 17:41:10 +0100

    Placeholder methods

--- src/Styles.Android/Bitmaps/BitmapExtensions.cs
+++ src/Styles.Android/Bitmaps/BitmapExtensions.cs
@@ -2,21 +2,26 @@
 
 namespace Styles
 {
-	public static class BitmapExtensions
-	{
-		public static Android.Graphics.Bitmap Resize(this Android.Graphics.Bitmap image, Android.Util.Size newSize)
-		{
-			throw new NotImplementedException();
-		}
+    public static class BitmapExtensions
+    {
+        public static Android.Graphics.Bitmap Resize(this Android.Graphics.Bitmap image, Android.Util.Size newSize)
+        {
+            throw new NotImplementedException();
+        }
 
-		public static Android.Graphics.Bitmap ToNative(this Bitmap bitmap)
-		{
-			throw new NotImplementedException();
-		}
+        public static Android.Graphics.Bitmap ToNative(this Bitmap bitmap)
+        {
+            throw new NotImplementedException();
+        }
 
-		public static Bitmap ToBitmap(this Android.Graphics.Bitmap image, Android.Util.Size scaledSize)
-		{
-			throw new NotImplementedException();
-		}
-	}
+        public static Bitmap ToBitmap(this Android.Graphics.Bitmap image)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Bitmap ToBitmap(this Android.Graphics.Bitmap image, Android.Util.Size scaledSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
