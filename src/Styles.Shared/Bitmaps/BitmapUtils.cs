6f444db934046bc3dcc4a88ac63756dbca3f3636
Clinton Francis <clfranci@microsoft.com>
2017-08-08 16:05:44 +0100

    Refactoring methods and updated nuspec

--- src/Styles.Shared/Bitmaps/BitmapUtils.cs
+++ src/Styles.Shared/Bitmaps/BitmapUtils.cs
@@ -4,178 +4,156 @@ using Styles;
 
 namespace Styles
 {
-	public class ColorSet
-	{
-		public ColorRGB BackgroundColor { get; set; }
-		public ColorRGB PrimaryColor { get; set; }
-		public ColorRGB SecondaryColor { get; set; }
-		public ColorRGB DetailColor { get; set; }
-	}
+    public static class BitmapUtils
+    {
+        // TODO implement methods
+        // colorWithAverageColorFromImage
 
-	public struct ColorCount
-	{
-		public ColorRGB Color { get; set; }
-		public int Count { get; set; }
+        // TODO rename and perhaps return a ColorScheme
+        public static ColorSet GetColorSet(this Bitmap bitmap)
+        {
+            var result = new ColorSet();
 
-		public ColorCount(ColorRGB color, int count)
-		{
-			Count = count;
-			Color = color;
-		}
-	}
+            var randomColorsThreshold = (bitmap.Height * 0.01);
+            var blackColor = ColorRGB.Black;
+            var whiteColor = ColorRGB.White;
 
-	public static class BitmapUtils
-	{
-		// TODO implement methods
-		// colorWithAverageColorFromImage
+            var leftEdgeColors = new List<ColorRGB>();
+            var imageColors = bitmap.GetColorArray();
 
-		// TODO rename and perhaps return a ColorScheme
-		public static ColorSet GetColorSet(this Bitmap bitmap)
-		{
-			var result = new ColorSet();
+            // Work out the edge colors
+            var count = 0;
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    // A lot of albums have white or black edges from crops, so ignore the first few pixels
+                    if (5 <= x && x <= 10)
+                    {
+                        leftEdgeColors.Add(imageColors[count]);
+                    }
+                    count++;
+                }
+            }
 
-			var randomColorsThreshold = (bitmap.Height * 0.01);
-			var blackColor = ColorRGB.Black;
-			var whiteColor = ColorRGB.White;
+            // TODO check which collection I should be looping
+            // Get background color
+            var sortedColors = new List<ColorCount>();
+            for (int i = 0; i < leftEdgeColors.Count; i++)
+            {
+                var colorCount = leftEdgeColors.FindAll((ColorRGB obj) => { return obj == leftEdgeColors[i]; }).Count;
+                if (randomColorsThreshold < colorCount)
+                {
+                    sortedColors.Add(new ColorCount(leftEdgeColors[i], colorCount));
+                }
+            }
 
-			var leftEdgeColors = new List<ColorRGB>();
-			var imageColors = bitmap.GetColorArray();
+            sortedColors.Sort(SortColors);
 
-			// Work out the edge colors
-			var count = 0;
-			for (int x = 0; x < bitmap.Width; x++)
-			{
-				for (int y = 0; y < bitmap.Height; y++)
-				{
-					// A lot of albums have white or black edges from crops, so ignore the first few pixels
-					if (5 <= x && x <= 10)
-					{
-						leftEdgeColors.Add(imageColors[count]);
-					}
-					count++;
-				}
-			}
+            ColorCount proposedEdgeColor;
+            if (sortedColors.Count > 0)
+            {
+                proposedEdgeColor = sortedColors[0];
+            }
+            else
+            {
+                proposedEdgeColor = new ColorCount(blackColor, 1);
+            }
 
-			// TODO check which collection I should be looping
-			// Get background color
-			var sortedColors = new List<ColorCount>();
-			for (int i = 0; i < leftEdgeColors.Count; i++)
-			{
-				var colorCount = leftEdgeColors.FindAll((ColorRGB obj) => { return obj == leftEdgeColors[i]; }).Count;
-				if (randomColorsThreshold < colorCount)
-				{
-					sortedColors.Add(new ColorCount(leftEdgeColors[i], colorCount));
-				}
-			}
+            if (proposedEdgeColor.Color.IsBlackOrWhite() && sortedColors.Count > 0)
+            {
+                for (int i = 0; i < sortedColors.Count; i++)
+                {
+                    var nextProposedEdgeColor = sortedColors[i];
+                    if ((float)nextProposedEdgeColor.Count / (float)proposedEdgeColor.Count > 0.3f)
+                    {
+                        if (!nextProposedEdgeColor.Color.IsBlackOrWhite())
+                        {
+                            proposedEdgeColor = nextProposedEdgeColor;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            result.BackgroundColor = proposedEdgeColor.Color;
 
-			sortedColors.Sort(SortColors);
+            // Get foreground colors
+            sortedColors.Clear();
+            var findDarkTextColor = !result.BackgroundColor.IsDarkColor();
 
-			ColorCount proposedEdgeColor;
-			if (sortedColors.Count > 0)
-			{
-				proposedEdgeColor = sortedColors[0];
-			}
-			else
-			{
-				proposedEdgeColor = new ColorCount(blackColor, 1);
-			}
+            foreach (var color in imageColors)
+            {
+                var colorMin = color.ColorWithMinimumSaturation(0.15f);
+                if (colorMin.IsDarkColor() == findDarkTextColor)
+                {
+                    var colorCount = leftEdgeColors.FindAll((ColorRGB obj) => { return obj == colorMin; }).Count;
+                    sortedColors.Add(new ColorCount(colorMin, colorCount));
+                }
+            }
 
-			if (proposedEdgeColor.Color.IsBlackOrWhite() && sortedColors.Count > 0)
-			{
-				for (int i = 0; i < sortedColors.Count; i++)
-				{
-					var nextProposedEdgeColor = sortedColors[i];
-					if ((float)nextProposedEdgeColor.Count / (float)proposedEdgeColor.Count > 0.3f)
-					{
-						if (!nextProposedEdgeColor.Color.IsBlackOrWhite())
-						{
-							proposedEdgeColor = nextProposedEdgeColor;
-							break;
-						}
-					}
-					else
-					{
-						break;
-					}
-				}
-			}
-			result.BackgroundColor = proposedEdgeColor.Color;
+            sortedColors.Sort(SortColors);
+            foreach (var countedColor in sortedColors)
+            {
+                var color = countedColor.Color;
 
-			// Get foreground colors
-			sortedColors.Clear();
-			var findDarkTextColor = !result.BackgroundColor.IsDarkColor();
+                // Set the primary color
+                if (result.PrimaryColor == ColorRGB.Empty)
+                {
+                    if (color.IsContrastingColor(result.BackgroundColor))
+                    {
+                        result.PrimaryColor = color;
+                    }
+                }
 
-			foreach (var color in imageColors)
-			{
-				var colorMin = color.ColorWithMinimumSaturation(0.15f);
-				if (colorMin.IsDarkColor() == findDarkTextColor)
-				{
-					var colorCount = leftEdgeColors.FindAll((ColorRGB obj) => { return obj == colorMin; }).Count;
-					sortedColors.Add(new ColorCount(colorMin, colorCount));
-				}
-			}
+                // Set the Secondary Color
+                else if (result.SecondaryColor == ColorRGB.Empty)
+                {
+                    if (!result.PrimaryColor.IsDistinct(color) || !color.IsContrastingColor(result.BackgroundColor))
+                    {
+                        continue;
+                    }
 
-			sortedColors.Sort(SortColors);
+                    result.SecondaryColor = color;
+                }
 
-			var noColor = ColorRGB.Empty;
-			foreach (var countedColor in sortedColors)
-			{
-				var color = countedColor.Color;
+                // Set the Detail Color
+                else if (result.DetailColor == ColorRGB.Empty)
+                {
+                    if (!result.SecondaryColor.IsDistinct(color) || !result.PrimaryColor.IsDistinct(color) || !color.IsContrastingColor(result.BackgroundColor))
+                    {
+                        continue;
+                    }
 
-				// Set the primary color
-				if (result.PrimaryColor == noColor)
-				{
-					if (color.IsContrastingColor(result.BackgroundColor))
-					{
-						result.PrimaryColor = color;
-					}
-				}
+                    result.DetailColor = color;
+                    break;
+                }
+            }
 
-				// Set the Secondary Color
-				else if (result.SecondaryColor == noColor)
-				{
-					if (!result.PrimaryColor.IsDistinct(color) || !color.IsContrastingColor(result.BackgroundColor))
-					{
-						continue;
-					}
+            var isDarkBackground = result.BackgroundColor.IsDarkColor();
+            if (result.PrimaryColor == ColorRGB.Empty)
+                result.PrimaryColor = isDarkBackground ? whiteColor : blackColor;
 
-					result.SecondaryColor = color;
-				}
+            if (result.SecondaryColor == ColorRGB.Empty)
+                result.SecondaryColor = isDarkBackground ? whiteColor : blackColor;
 
-				// Set the Detail Color
-				else if (result.DetailColor == noColor)
-				{
-					if (!result.SecondaryColor.IsDistinct(color) || !result.PrimaryColor.IsDistinct(color) || !color.IsContrastingColor(result.BackgroundColor))
-					{
-						continue;
-					}
+            if (result.DetailColor == ColorRGB.Empty)
+                result.DetailColor = isDarkBackground ? whiteColor : blackColor;
 
-					result.DetailColor = color;
-					break;
-				}
-			}
+            return result;
+        }
 
-			var isDarkBackground = result.BackgroundColor.IsDarkColor ();
-			if (result.PrimaryColor == ColorRGB.Empty)
-				result.PrimaryColor = isDarkBackground ? whiteColor : blackColor;
-
-			if (result.SecondaryColor == ColorRGB.Empty)
-				result.SecondaryColor = isDarkBackground ? whiteColor : blackColor;
-
-			if (result.DetailColor == ColorRGB.Empty)
-				result.DetailColor = isDarkBackground ? whiteColor : blackColor;
-
-			return result;
-		}
-
-		private static int SortColors(ColorCount x, ColorCount y)
-		{
-			if (x.Count < y.Count)
-				return -1;
-			if (x.Count == y.Count)
-				return 0;
-			return 1;
-		}
-	}
+        private static int SortColors(ColorCount x, ColorCount y)
+        {
+            if (x.Count < y.Count)
+                return -1;
+            if (x.Count == y.Count)
+                return 0;
+            return 1;
+        }
+    }
 }
 
